4dfb5f49d009a6c78db3215e49f91f843ba13e83
Ali <sobirov.px@gmail.com>
2024-05-21 16:17:32 +0300

    Update HomeWork.py

--- HomeWork.py
+++ HomeWork.py
@@ -1,55 +1,26 @@
-import psycopg2
+class FibonacciIterator:
+    def __init__(self, n):
+        self.n = n
+        self.current = 0
+        self.next = 1
+        self.count = 0
 
-db_name = 'n47'
-password = '0060'
-host = 'localhost'
-port = 5432
-user = 'postgres'
+    def __iter__(self):
+        return self
 
-conn = psycopg2.connect(database=db_name, user=user, password=password, host=host, port=port)
-
-cur = conn.cursor()
-
-
-def create_table():
-    products_table = """
-    CREATE TABLE IF NOT EXISTS products(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(55) NOT NULL,
-    image VARCHAR(155),
-    is_liked BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );"""
-    cur.execute(products_table)
-    conn.commit()
-    print("Table created successfully")
+    def __next__(self):
+        if self.count >= self.n:
+            raise StopIteration
+        result = self.current
+        self.current, self.next = self.next, self.current + self.next
+        self.count += 1
+        return result
 
 
-def insert_product(name, image):
-    insert_product_data = """insert into products(name,image) values(%s,%s);"""
-    cur.execute(insert_product_data, (name, image))
-    conn.commit()
-    print("Product inserted successfully")
-
-
-def select_products(_id):
-    select_data = """select * from products where id = %s """
-    cur.execute(select_data, (_id,))
-    cur.fetchone()
-    products = cur.fetchall()
-    return products
-
-
-def delete_product(_id):
-    delete_data = """delete from products where id = %s"""
-    cur.execute(delete_data, (_id,))
-    conn.commit()
-    print("Product deleted successfully")
-
-
-def update_product(_id, new_name, new_image):
-    update_data = """update products set name = %s, image = %s where id = %s;"""
-    cur.execute(update_data, (new_name, new_image, _id))
-    conn.commit()
-    print("Product updated successfully")
+fibonacci = FibonacciIterator(10)
+while True:
+    try:
+        num = next(fibonacci)
+        print(num)
+    except StopIteration:
+        break
