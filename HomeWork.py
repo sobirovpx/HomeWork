9a32df12364873f4354406a2bba2e437d587a67c
Ali <sobirov.px@gmail.com>
2024-05-25 16:18:25 +0300

    Update HomeWork.py

--- HomeWork.py
+++ HomeWork.py
@@ -1,53 +1,23 @@
-class Fibonacci:
-    def __init__(self):
-        self.prev = 0
-        self.curr = 1
+import time
 
-    def __iter__(self):
+
+class TimerContextManager:
+    def __init__(self):
+        self.start_time = None
+
+    def __enter__(self):
+        self.start_time = time.time()
         return self
 
-    def __next__(self):
-        value = self.curr
-        self.prev, self.curr = self.curr, self.prev + self.curr
-        return value
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        end_time = time.time()
+        elapsed_time = end_time - self.start_time
+        print(f"Time taken: {elapsed_time} seconds")
 
 
-fib = Fibonacci()
-# for i in range(10):
-#     print(next(fib))
-
-# try:
-#     while True:
-#         print(next(fib))
-# except StopIteration:
-#     pass
-
-
-def fibonacci_gen():
-    prev, curr = 0, 1
-    while True:
-        yield curr
-        prev, curr = curr, prev + curr
-
-
-fib2 = fibonacci_gen()
-
-
-def programming_languages_gen():
-    try:
-        while True:
-            language = yield
-            print("Programming language: " + language)
-    except GeneratorExit:
-        print("generator closed")
-
-
-gen = programming_languages_gen()
-next(gen)
-
-languages = ["Python", "Java", "C", "C++", "JavaScript", "Ruby", "PHP", "Swift", "Go", "Rust"]
-
-for i in languages:
-    gen.send(i)
-
-gen.close()
+with TimerContextManager():
+    result = 1
+    for i in range(1, 100001):
+        result *= i
+    print(result)
+    
